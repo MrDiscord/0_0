0134c1ba49aa9f5488230c551968d0d98225fd7c
MrDiscord <136884295+MrDiscord@users.noreply.github.com>
2023-07-18 09:53:06 +0300

    Add files via upload

---
+++ sketch_jul17a.ino
@@ -0,0 +1,25 @@
+#include <Display.h>
+
+Display display(UART1);
+
+void setup()
+{
+}
+
+void loop()
+{
+  display.clear();
+  display.text(0, 0, 2, WHITE, "I Robot!");	
+  delay( 1000 );
+  display.clear();
+  display.text(0, 0, 2, GREEN, "Motor Start");
+  display.circleFilled(160, 120, 50, BLUE);	
+  delay( 1000 );
+  startMotor(4, 100);
+  delay( 3000 );
+  display.clear();
+  display.text(0, 0, 2, RED, "Motor Stop");	
+  delay( 1000 );
+  stopMotor(4);
+}
+
